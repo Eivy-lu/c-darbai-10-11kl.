dda098cff129ff8d69a4169d0fad236f942db4e2
Ievut <156522172+Eivy-lu@users.noreply.github.com>
2025-01-29 20:11:49 +0200

    Add files via upload

---
+++ Užduotis 3.1. 14. Sveikojo skaičiaus skaitmenų suma.cpp
@@ -0,0 +1,33 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int SkSum (int n);
+
+int main()
+
+{
+  int n;
+  cin >> n;
+
+  for (int i = 0; i < n; i++)
+  {
+      int sk;
+      cin >> sk;
+
+      cout << sk <<" "<<SkSum(sk)<<endl;
+  }
+
+  return 0;
+}
+int SkSum (int n)
+{
+    int suma = 0;
+    while (n > 0)
+    {
+        suma += n%10;
+        n /= 10;
+    }
+
+    return suma;
+}
