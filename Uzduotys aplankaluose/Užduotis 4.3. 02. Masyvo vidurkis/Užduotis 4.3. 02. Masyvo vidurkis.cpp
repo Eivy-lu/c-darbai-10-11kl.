e2b95d1992d4cb6466a8b60b880a68311fffaa87
Ievut <156522172+Eivy-lu@users.noreply.github.com>
2025-03-06 19:01:49 +0200

    Add files via upload

---
+++ Uzduotys aplankaluose/Užduotis 4.3. 02. Masyvo vidurkis/Užduotis 4.3. 02. Masyvo vidurkis.cpp
@@ -0,0 +1,53 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+void skaitymas(int masyvas[][10], int &n, int &m);
+double radimas(int masyvas[][10], int &n, int &m);
+void spausdinimas(double suma);
+
+int main()
+
+{
+    int masyvas [10][10];
+    int n, m;
+
+    skaitymas (masyvas, n, m);
+    spausdinimas(radimas(masyvas, n, m));
+
+    return 0;
+}
+
+void spausdinimas(double suma)
+{
+    ofstream rez("U1rez.txt");
+    rez <<fixed<<setprecision(2)<< suma;
+}
+
+double radimas(int masyvas[][10], int &n, int &m)
+{
+    double suma = 0;
+    for(int i = 0; i < n; i++)
+    {
+        for(int j = 0; j < m; j++)
+        {
+            suma += masyvas[i][j];
+        }
+    }
+    return suma/(n*m);
+}
+
+void skaitymas(int masyvas[][10], int &n, int &m)
+{
+    ifstream sk("U1.txt");
+
+    sk >> n >> m;
+
+    for(int i = 0; i < n; i++)
+    {
+        for(int j = 0; j < m; j++)
+        {
+            sk >> masyvas[i][j];
+        }
+    }
+}
